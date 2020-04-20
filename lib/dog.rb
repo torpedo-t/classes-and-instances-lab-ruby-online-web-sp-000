a536c4ee63131c78cbec8f24391e839eb6d316eb
Thomas John Helin <thomashelin@yahoo.com>
2020-04-20 18:34:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,13 @@ class Dog
   #body
 end
 
-capser = Dog.new
-cash = Dog.new
-scooter = Dog.new
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
+
+class Person
+  #body
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
